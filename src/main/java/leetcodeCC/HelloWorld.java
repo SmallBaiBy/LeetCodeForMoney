6bd6e2dfbd70b1489d9c2b6d2e15226ad39fd6de
yang943762359 <18720540427@163.com>
2021-05-16 22:52:28 +0800

    内卷之王--yzh

---
+++ src/main/java/leetcodeCC/HelloWorld.java
@@ -0,0 +1,11 @@
+package leetcodeCC;
+
+/**
+ * @author SmallBai
+ * date: 2021/5/16 22:39
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
